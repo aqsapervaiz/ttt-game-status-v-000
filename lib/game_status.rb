80597bea3b8675ec959346555fcb0a969379a61c
Aqsa Pervaiz <aqsapervaiz@gmail.com>
2020-08-29 18:53:53 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,73 +4,66 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [ 
-  [0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [2,4,6]
-  ]
-  
-  #define won argument
-  
-  def won?(board)
-    WIN_COMBINATIONS.each { |win_combo|
-     index_0 = win_combo[0]
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+]
+
+def won?(board)
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
     index_1 = win_combo[1]
     index_2 = win_combo[2]
-    
+
     position_1 = board[index_0]
     position_2 = board[index_1]
     position_3 = board[index_2]
-    
+
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combo
-    elseif position_1 == "O" && position_2 == "O" && position_3 == "O"
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combo
     end
-    }
-    return false 
+  }
+  return false
+end
+
+def full?(board)
+  board.all? {|index| index == "X" || index == "O"}
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
   end
- 
-  
-  def full?(board)
-    board.all {|index| index == "X" || index == "O"}
+end
+
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    return true
+  else
+    return false
   end
-  
-  
-  def draw?(board)
-    if !won?(board) && full?(board)
-      return true 
-    else 
-      return false
-    end
-  end
-  
-  
-  def over?(board)
-    if won?(board) || full?(board) || draw?(board)
-      return true
+end
+
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
     else
-      return false 
-    end
-  end
-  
-  
-  def winner (board)
-    index = []
-    index = won?(board)
-    if index == false 
       return "O"
-    else
-      if board[index[0]] == "X"
-        return nil
-      else 
-        return "O"
-      end
     end
   end
-    
+end
